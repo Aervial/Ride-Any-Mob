39a9a7bee247b63a35f0fd446c3181a88a5dd4b3
Aervial <aervial34@gmail.com>
2026-02-18 16:16:21 -0500

    Initialized and created the mod

---
+++ src/client/java/aervial/rideanymob/RideAnyMobClient.java
@@ -0,0 +1,10 @@
+package aervial.rideanymob;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RideAnyMobClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
